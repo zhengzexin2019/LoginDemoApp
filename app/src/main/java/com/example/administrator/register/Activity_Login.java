42ea162cb28d605e01caa27c93ed3b4275e15223
zhengzexin2019 <50038968+zhengzexin2019@users.noreply.github.com>
2019-04-30 08:25:28 +0800

    17023148_郑泽昕

---
+++ app/src/main/java/com/example/administrator/register/Activity_Login.java
@@ -0,0 +1,24 @@
+package com.example.administrator.register;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succeed);
+        Button button2 = findViewById(R.id.btn2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/administrator/register/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.administrator.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = ((EditText) findViewById(R.id.username)).getText().toString();//获取用户名输入框中的信息
+                String pwd = ((EditText) findViewById(R.id.password)).getText().toString();//获取密码输入框中的信息
+                if (!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(pwd)) {
+                    login(userName, pwd);
+                } else {
+                    Toast.makeText(MainActivity.this, "用户名和密码不能为空", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login(final String userName, final String pwd) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (userName.equals("17023148") && pwd.equals("password17023148")) {//判断用户名是否为17023148，密码是否为password17023148
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent();
+                            intent.setClass(MainActivity.this, Activity_Login.class);
+                            startActivity(intent);
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+}
+
+
